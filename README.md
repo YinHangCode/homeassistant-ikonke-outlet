2f3ae588d692dc2113742d4d259b6c6c84170766
YinHangCode <141564@qq.com>
2017-09-20 01:31:13 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,47 @@
+# homeassistant-ikonke-outlet
+
+HomeAssistant的控客插座插件。   
+   
+**注: 如果有bug请提交到 [issues](https://github.com/YinHangCode/homeassistant-ikonke-outlet/issues) 或 [QQ群: 107927710](//shang.qq.com/wpa/qunwpa?idkey=8b9566598f40dd68412065ada24184ef72c6bddaa11525ca26c4e1536a8f2a3d)。**   
+
+![](https://raw.githubusercontent.com/YinHangCode/homeassistant-ikonke-outlet/master/images/K2.jpg)
+![](https://raw.githubusercontent.com/YinHangCode/homeassistant-ikonke-outlet/master/images/K2Pro.jpg)
+![](https://raw.githubusercontent.com/YinHangCode/homeassistant-ikonke-outlet/master/images/MiniB.jpg)
+
+## 支持的设备
+1.K2   
+2.K2 Pro   
+3.K Mini Pro   
+
+## 安装说明
+1.安装HomeAssistant。   
+2.安装[ikonkeIO](https://github.com/YinHangCode/ikonkeIO)。   
+3.将homeassistant-ikonke-outlet.py文件放到~/.homeassistant/custom_components/switch/下。
+
+## 配置说明
+配置"ikonkeIO"为ikonkeIO目录下sh文件的绝度路径。   
+设备的"type"、"ip"、"mac"、"password"可以通过ikonkeIO获取，具体参考[ikonkeIO](https://github.com/YinHangCode/ikonkeIO)项目。   
+示例如下：   
+```
+switch:
+  - platform: homeassistant-ikonke-outlet
+    ikonkeIO: '/home/pi/node_modules/ikonkeIO/ikonkeIO.sh'
+    deviceCfgs:
+      - type: 'k2pro'
+        ip: '192.168.88.42'
+        mac: '28-d0-8a-08-79-4d'
+        password: '36629'
+      - type: 'k2'
+        ip: '192.168.88.43'
+        mac: '28-d0-8a-02-3f-e6'
+        password: '88663'
+      - type: 'mini_b'
+        ip: '192.168.88.41'
+        mac: '28-d0-8a-81-77-5f'
+        password: 'eA,-J=57'
+```
+## 版本更新记录
+### 0.0.1
+1.支持控制K2设备.   
+2.支持控制K2 Pro设备.   
+3.支持控制K Mini Pro设备.   
---
+++ custom_components/switch/homeassistant-ikonke-outlet.py
@@ -0,0 +1,107 @@
+import os
+
+from homeassistant.components.switch import SwitchDevice
+from homeassistant.const import DEVICE_DEFAULT_NAME
+
+platformVersion = "0.0.1"
+
+# pylint: disable=unused-argument
+def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+    print("[IkonkeOutletPlatform][INFO]*********************************************************************")
+    print("[IkonkeOutletPlatform][INFO]                IkonkeOutletPlatform v%s By YinHang"%(platformVersion))
+    print("[IkonkeOutletPlatform][INFO]  GitHub: https://github.com/YinHangCode/homeassistant_ikonke_outlet ")
+    print("[IkonkeOutletPlatform][INFO]                                                 QQ Group: 107927710 ")
+    print("[IkonkeOutletPlatform][INFO]*********************************************************************")
+    print("[IkonkeOutletPlatform][INFO]start success...")
+    
+    addDeviceArr = []
+    
+    ikonkeIO = config.get("ikonkeIO")
+    deviceCfgs = config.get("deviceCfgs")
+    for deviceCfg in deviceCfgs:
+            type = deviceCfg.get("type")
+            if type == "k2":
+                addDeviceArr.append(\
+                    K2Outlet("k2_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+            elif type == "k2pro":
+                addDeviceArr.append(\
+                    K2ProOutlet("k2pro_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+            elif type == "mini_b":
+                addDeviceArr.append(\
+                    MiniBOutlet("mini_b_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+            else:
+                print("[IkonkeOutletPlatform][ERROR]error type" + type)
+    
+    add_devices_callback(addDeviceArr)
+
+class IkonkeOutletBase(SwitchDevice):
+    ikonkeIO = ''
+    type = ''
+    ip = ''
+    mac = ''
+    passwd = ''
+    
+    def __init__(self, name, icon, ikonkeIO, type, ip, mac, passwd):
+        self._name = name or DEVICE_DEFAULT_NAME
+        self._icon = icon
+        self._state = False
+        
+        self.ikonkeIO = ikonkeIO
+        self.type = type
+        self.ip = ip
+        self.mac = mac
+        self.passwd = passwd
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def icon(self):
+        return self._icon
+    
+    @property
+    def should_poll(self):
+        return True
+    
+    @property
+    def is_on(self):
+        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' getRelay'
+        result = os.popen(command).read().strip('\n')
+        if result == 'open':
+            self._state = True
+        elif result == 'close':
+            self._state = False
+        else:
+            self._state = False
+        
+        return self._state
+    
+    def turn_on(self, **kwargs):
+        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' setRelay open'
+        result = os.popen(command).read().strip('\n')
+        if result == 'success':
+            self._state = True
+            self.schedule_update_ha_state()
+        else:
+            pass
+    
+    def turn_off(self, **kwargs):
+        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' setRelay close'
+        result = os.popen(command).read().strip('\n')
+        if result == 'success':
+            self._state = False
+            self.schedule_update_ha_state()
+        else:
+            pass
+
+class K2Outlet(IkonkeOutletBase):
+    pass
+
+class K2ProOutlet(IkonkeOutletBase):
+    pass
+
+class MiniBOutlet(IkonkeOutletBase):
+    pass
+
+
