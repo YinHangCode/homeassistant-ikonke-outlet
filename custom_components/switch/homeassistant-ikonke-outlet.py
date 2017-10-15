8f3d2e50619aebd4fe6f8a0bee6cc75db93fd343
YinHangCode <141564@qq.com>
2017-10-15 22:48:53 +0800

    0.0.3

--- custom_components/switch/homeassistant-ikonke-outlet.py
+++ custom_components/switch/homeassistant-ikonke-outlet.py
@@ -5,7 +5,6 @@ from homeassistant.const import DEVICE_DEFAULT_NAME
 
 platformVersion = "0.0.2"
 
-# pylint: disable=unused-argument
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     print("[IkonkeOutletPlatform][INFO]*********************************************************************")
     print("[IkonkeOutletPlatform][INFO]                IkonkeOutletPlatform v%s By YinHang"%(platformVersion))
@@ -19,18 +18,18 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     ikonkeIO = config.get("ikonkeIO")
     deviceCfgs = config.get("deviceCfgs")
     for deviceCfg in deviceCfgs:
-            type = deviceCfg.get("type")
-            if type == "k2":
-                addDeviceArr.append(\
-                    K2Outlet("k2_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
-            elif type == "k2pro":
-                addDeviceArr.append(\
-                    K2ProOutlet("k2pro_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
-            elif type == "mini_b":
-                addDeviceArr.append(\
-                    MiniBOutlet("mini_b_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
-            else:
-                print("[IkonkeOutletPlatform][ERROR]error type" + type)
+        type = deviceCfg.get("type")
+        if type == "k2":
+            addDeviceArr.append(\
+                K2Outlet("k2_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+        elif type == "k2pro":
+            addDeviceArr.append(\
+                K2ProOutlet("k2pro_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+        elif type == "mini_b":
+            addDeviceArr.append(\
+                MiniBOutlet("mini_b_" + deviceCfg.get("mac").replace('-',''), 'mdi:power-socket', ikonkeIO, deviceCfg.get("type"), deviceCfg.get("ip"), deviceCfg.get("mac"), deviceCfg.get("password")))
+        else:
+            print("[IkonkeOutletPlatform][ERROR]error type" + type)
     
     add_devices_callback(addDeviceArr)
 
@@ -72,7 +71,7 @@ class IkonkeOutletBase(SwitchDevice):
         return True
     
     def update(self):
-        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' getRelay'
+        command = 'sh "{ikonkeio}" -C "{type}" "{ip}" "{mac}" "{password}" getRelay'.format(ikonkeio=self.ikonkeIO, type=self.type, ip=self.ip, mac=self.mac, password=self.passwd)
         result = os.popen(command).read().strip('\n')
         if result == 'open':
             self._state = True
@@ -88,7 +87,7 @@ class IkonkeOutletBase(SwitchDevice):
         return self._state
     
     def turn_on(self, **kwargs):
-        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' setRelay open'
+        command = 'sh "{ikonkeio}" -C "{type}" "{ip}" "{mac}" "{password}" setRelay open'.format(ikonkeio=self.ikonkeIO, type=self.type, ip=self.ip, mac=self.mac, password=self.passwd)
         result = os.popen(command).read().strip('\n')
         if result == 'success':
             self._state = True
@@ -97,7 +96,7 @@ class IkonkeOutletBase(SwitchDevice):
             pass
     
     def turn_off(self, **kwargs):
-        command = 'sh ' + self.ikonkeIO + ' -C ' + self.type + ' ' + self.ip + ' ' + self.mac + ' ' + self.passwd + ' setRelay close'
+        command = 'sh "{ikonkeio}" -C "{type}" "{ip}" "{mac}" "{password}" setRelay close'.format(ikonkeio=self.ikonkeIO, type=self.type, ip=self.ip, mac=self.mac, password=self.passwd)
         result = os.popen(command).read().strip('\n')
         if result == 'success':
             self._state = False
